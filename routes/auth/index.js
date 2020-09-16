946076a31a61abeb41b658649d57e57281066fb2
kjk <kkole3897@naver.com>
2020-09-16 18:16:42 +0900

    logout 미들웨어 구현
    logout하게 되면 session을 destroy하는 방식으로 구현
    login 상태인 경우에만 가능하도록 함

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const login = require('./login');
+const logout = require('./logout');
 
 router.use('/', login);
+router.use('/', logout);
 
 module.exports = router;
