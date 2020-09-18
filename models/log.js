baa54d98e58d1ef00765f94b74ea23828dfd88a2
kjk <kkole3897@naver.com>
2020-09-19 00:32:53 +0900

    getLogsByUserId() 함수 추가
    DB에서 user_id로 기록된 모든 log 데이터를 가져오는 함수를 만들어서
    나중에 만들 log관련 api를 만들 때 사용할 수 있도록 한다.

---
+++ models/log.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'todo',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+function getLogsByUserId(userId) {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            `SELECT user_id, action, card_description, from_title, to_title, record_time
+            FROM Log
+            WHERE user_id = ?;`,
+            [userId],
+            (err, results, fields) => {
+                try {
+                    resolve(results);
+                } catch {
+                    reject(err);
+                }
+            }
+        );
+    });
+}
+
+module.exports = {
+    getLogsByUserId
+};
+
