f263f8da17b6b33758bc99e4ee90caf6e04df26c
kjk <kkole3897@naver.com>
2020-09-20 20:28:12 +0900

    [리팩토링] card api
    card router과 controller을 나눠서 서비스로직을 controller에서 처리하고,
    router는 api를 어떤 경로로 사용할지 정의하도록 변경

--- routes/card/card.js
+++ routes/card/card.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const router = express.Router();
 const models = require('../../models');
 const card = models.card;
 
-router.get('/', (req, res, next) => {
+function getCards(req, res, next) {
     const userId = req.session.userId;
     card.getCardsByUserId(userId).then((results) => {
-        res.send(results);
+        res.send({status: 'ok', message: '카드 데이터를 불러왔습니다.'});
     }).catch((err) => {
-        console.error(err);
-        next();
+        res.send({status: 'fail', message: '카드 데이터를 불러오지 못했습니다.'});
     });
-}).post('/', (req, res, next) => {
+}
+
+function addCard(req, res, next) {
     const listId = req.body.listId;
     const description = req.body.description;
-
     card.addCard(listId, description).then((results) => {
-        res.send('카드 추가 성공');
+        res.send({status: 'ok', message: '카드를 추가했습니다.'});
     }).catch((err) => {
-        res.send('카드 추가 실패');
+        res.send({status: 'fail', message: '카드를 추가하지 못했습니다.'});
     });
-}).put('/', (req, res, next) => {
+}
+
+function updateCard(req, res, next) {
     const cardId = req.body.cardId;
     const listId = req.body.listId;
     const description = req.body.description;
     const removed = req.body.removed;
 
-    console.log(cardId);
-    console.log(listId);
-    console.log(description);
-    console.log(removed);
     card.updateAllValues(cardId, listId, description, removed).then((results) => {
-        res.send('업데이트 성공');
+        res.send({status: 'ok', message: '카드를 업데이트했습니다.'});
     }).catch((err) => {
-        res.send('업데이트 실패');
+        res.send({status: 'fail', message: '카드를 업데이트하지 못했습니다.'});
     });
-});
+}
 
-module.exports = router;
+module.exports = {
+    getCards,
+    addCard,
+    updateCard
+};
--- routes/card/index.js
+++ routes/card/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const card = require('./card');
 
-router.use('/', card);
+router.get('/', card.getCards);
+router.post('/', card.addCard);
+router.put('/', card.updateCard);
 
 module.exports = router;
