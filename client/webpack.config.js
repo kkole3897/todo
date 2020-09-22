61af88d5062b3ad8cc496c976f3711223d74ecfe
kjk <kkole3897@naver.com>
2020-09-22 09:08:38 +0900

    프론트엔드 개발환경 설정
    webpack, babel 이용해서 server/public에 빌드 결과 생성하도록 설정
    
    html-webpack-plugin, style-loader, css-loader, file-loader 사용

---
+++ client/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../../server/public')
+  },
+  devServer: {
+      port: 8080,
+      hot: true
+  },
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: 'node_modules',
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['env']
+                  }
+              }
+          },
+          {
+              test: /\.css$/,
+              use: ['style-loader', 'css-loader']
+          },
+          {
+              test: /\.(png|jp(e*)g)$/,
+              use: 'file-loader'
+          }
+      ]
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: 'index.html'
+      })
+  ]
+};
