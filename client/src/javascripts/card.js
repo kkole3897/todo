121e22ce5e783c51704aee19d8113818f3d0a310
kjk <kkole3897@naver.com>
2020-09-25 11:34:56 +0900

    card input form에 css 적용
    layout, margin, textarea와 button 크기 조정, button 색상과 모양 변경

--- client/src/javascripts/card.js
+++ client/src/javascripts/card.js
@@ -2,9 +2,13 @@ function renderCardInput(target) {
     const cardInputForm = `
         <div class="card-input">
             <form class="card-input__form" action="#" method="post">
-                <textarea name="description" type="text" placeholder="Enter a note"></textarea>
-                <input type="submit" value="Add">
-                <input type="button", value="Cancel">
+                <div>
+                    <textarea class="card-input__textarea" name="description" type="text" placeholder="Enter a note"></textarea>
+                </div>
+                <div class="card-input__button-container">
+                    <input class="card-input__button card-input__button--margin-right card-input__button--green" type="submit" value="Add">
+                    <input class="card-input__button card-input__button--light-gray" type="button", value="Cancel">
+                </div>
             </form>
         </div>
         `;
