29d5688feee4e604c67cddea731fc14b1438bfe6
kjk <kkole3897@naver.com>
2021-01-28 15:06:58 +0900

    feat: Board 컴포넌트 추가
    - Board 상단에 있는 UI 추가
    - PlusButton, MoreButton, Count, Title 추가
    - PlushButton에 사용되는 PlusIcon을 plus.svg로 사용
    - MoreButton에 사용되는 MoreIcon은 more.svg로 사용

---
+++ client/src/components/Board.js
@@ -0,0 +1,50 @@
+import './Board.css';
+import PlusIcon from '../assets/plus.svg';
+import MoreIcon from '../assets/more.svg';
+
+function Board({ title = '' }) {
+  const $board = document.createElement('div');
+  $board.className = 'board';
+
+  const $boardInner = document.createElement('div');
+  $boardInner.className = 'board__inner';
+
+  const $boardHeader = document.createElement('div');
+  $boardHeader.className = 'board__header';
+
+  const $leftDiv = document.createElement('div');
+  $leftDiv.className = 'flex';
+
+  const $rightDiv = document.createElement('div');
+  $rightDiv.className = 'flex';
+
+  const $cardCount = document.createElement('div');
+  $cardCount.className = 'board__card-count';
+  $cardCount.innerHTML = 0;
+
+  const $title = document.createElement('div');
+  $title.className = 'board__title board__title--ml';
+  $title.innerHTML = title;
+
+  const $addCardButton = document.createElement('div');
+  $addCardButton.className =
+    'board__add-card-button board__add-card-button--mr';
+  $addCardButton.innerHTML = `<img src=${PlusIcon} />`;
+
+  const $moreButton = document.createElement('div');
+  $moreButton.className = 'board__more-button';
+  $moreButton.innerHTML = `<img src=${MoreIcon} />`;
+
+  $leftDiv.appendChild($cardCount);
+  $leftDiv.appendChild($title);
+  $rightDiv.appendChild($addCardButton);
+  $rightDiv.appendChild($moreButton);
+  $boardHeader.appendChild($leftDiv);
+  $boardHeader.appendChild($rightDiv);
+  $boardInner.appendChild($boardHeader);
+  $board.appendChild($boardInner);
+
+  return $board;
+}
+
+export default Board;
