0c66d70f08bdc8a33cdf1202e538c94fcae6ab2e
kjk <kkole3897@naver.com>
2021-01-28 17:28:13 +0900

    feat: AddCardForm 추가
    AddCardForm의 기본적인 UI 구성해서 추가
    - textarea, add, cancel 버튼 추가

---
+++ client/src/components/AddCardForm.js
@@ -0,0 +1,32 @@
+import './AddCardForm.css';
+
+function AddCardForm() {
+  const $addCardForm = document.createElement('form');
+  $addCardForm.className = 'add-card-form add-card-form--m';
+
+  const $textArea = document.createElement('textarea');
+  $textArea.className = 'add-card-form__textarea';
+  $textArea.placeholder = 'Enter a note';
+
+  const $buttonContainer = document.createElement('div');
+  $buttonContainer.className =
+    'add-card-form__button-container add-card-form__button-container--m';
+
+  const $addButton = document.createElement('button');
+  $addButton.className =
+    'add-card-form__add-button add-card-form__add-button--mr';
+  $addButton.innerHTML = 'Add';
+
+  const $cancelButton = document.createElement('button');
+  $cancelButton.className = 'add-card-form__cancel-button';
+  $cancelButton.innerHTML = 'Cancel';
+
+  $buttonContainer.appendChild($addButton);
+  $buttonContainer.appendChild($cancelButton);
+  $addCardForm.appendChild($textArea);
+  $addCardForm.appendChild($buttonContainer);
+
+  return $addCardForm;
+}
+
+export default AddCardForm;
