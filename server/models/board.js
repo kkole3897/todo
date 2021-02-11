8b28146d4af3747729aae175642f01f146f9e4f8
KimJinKwan <kkole3897@naver.com>
2021-02-11 16:28:34 +0900

    refactor: List를 Board로 변경
    - list 테이블을 board 테이블로 변경
    - pool을 하나만 사용할 수 있도록 class 생성자에 넘겨서 사용
    - 새로운 board를 추가하는 addBoard() 메소드 추가

---
+++ server/models/board.js
@@ -0,0 +1,40 @@
+class Board {
+  constructor(database) {
+    this.database = database;
+
+    const createBoardTableQuery = `
+      CREATE TABLE IF NOT EXISTS board(
+        id INT AUTO_INCREMENT,
+        name VARCHAR(50) NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+        deleted_at DATETIME,
+        user_id VARCHAR(20),
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES user(id),
+        UNIQUE KEY unique_board_name_by_user (name, user_id)
+      );
+    `;
+    this.database.query(createBoardTableQuery, (err, result) => {
+      // console.log(result);
+    });
+  }
+
+  async addBoard({ name, userId }) {
+    if (!this.validateName(name)) {
+      throw new Error('invalid name');
+    }
+    const addBoardQuery = `
+      INSERT INTO board(name, user_id)
+      VALUES (?, ?);
+    `;
+    const [result] = await this.database.query(addBoardQuery, [name, userId]);
+    return { id: result.insertId };
+  }
+
+  validateName(name) {
+    return 0 < name.length && name.length <= 50;
+  }
+}
+
+module.exports = Board;
