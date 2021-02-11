2e5494249cd48e5c7eb5c72dfcf19555e1b3190f
KimJinKwan <kkole3897@naver.com>
2021-02-11 19:53:42 +0900

    refactor: model의 entry 수정
    모델을 불러오고 내보내는 방식을 리팩토링해서 변경된 수정 사항들을
    반영해서 업데이트

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,26 @@
-const user = require('./user');
-const card = require('./card')
-const list = require('./list');
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const User = require('./user');
+const Board = require('./board');
+const Card = require('./card');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'todo_test',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const user = new User(pool);
+const board = new Board(pool);
+const card = new Card(pool);
 
 module.exports = {
-    user,
-    card,
-    list
+  userModel: user,
+  boardModel: board,
+  cardModel: card,
 };
