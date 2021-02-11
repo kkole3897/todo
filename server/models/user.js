a6137647b85a3751ca34d4132435f78fac6c509f
KimJinKwan <kkole3897@naver.com>
2021-02-11 14:44:00 +0900

    refactor: user 데이터베이스 수정
    - validateId(), validatePassword()로 id와 password를 검증
    - addUser() 메소드 추가
    - getUser() 메소드 추가
    - addUser()에서 id, password를 검증하고 추가할 수 있도록 함

--- server/models/user.js
+++ server/models/user.js
@@ -1,46 +1,50 @@
-const mysql = require('mysql2');
+class Users {
+  constructor(database) {
+    this.database = database;
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'todo',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+    const createUserTableQuery = `
+      CREATE TABLE IF NOT EXISTS user(
+        id varchar(20),
+        password varchar(16) NOT NULL,
+        PRIMARY KEY(id)
+      );
+    `;
+    this.database.query(createUserTableQuery, (err, results) => {
+      // console.log(results);
+    });
+  }
 
-function findUserById(id) {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            'SELECT user_id FROM `User` WHERE `user_id` = ?',
-            [id],
-            (err, results, fields) => {
-                if (results.length > 0) resolve(results[0]['user_id']);
-                else resolve(undefined);
-                reject(err);
-            }
-        )
-    })
+  async addUser({ id, password }) {
+    if (!this.validateId(id) || !this.validatePassword(password)) {
+      throw new Error('invalid id or password');
+    }
+    const addUserQuery = `
+      INSERT INTO user(id, password)
+      VALUES (?, ?);
+    `;
+    await this.database.query(addUserQuery, [id, password]);
+    return { id };
+  }
+
+  async getUser({ id, password }) {
+    const getUserQuery = `
+        SELECT id
+        FROM user
+        WHERE id = ? AND password = ?;
+      `;
+    const [rows] = await this.database.query(getUserQuery, [id, password]);
+    return { ...rows[0] };
+  }
+
+  validateId(id) {
+    const pattern = /^[a-z0-9][a-z0-9\-_]{4,19}$/;
+    return pattern.test(id);
+  }
+
+  validatePassword(password) {
+    const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[~`!@#$%^&*()\-_=+{}\[\]\\|:;'",<.>/?]).{8,16}$/;
+    return pattern.test(password);
+  }
 }
 
-function insertUserId(id) {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            'INSERT INTO User VALUES(?)',
-            [id],
-            (err, results, fields) => {
-                try {
-                    resolve(results);
-                } catch {
-                    reject(err);
-                }
-            }
-        )
-    })
-}
-
-module.exports = {
-    findUserById,
-    insertUserId
-};
+module.exports = Users;
