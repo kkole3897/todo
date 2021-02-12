e6ce3f2241a3baf5187f5075cc35127ac26035dd
KimJinKwan <kkole3897@naver.com>
2021-02-12 16:52:04 +0900

    feat: signup() 메소드 추가
    - 회원가입할 때 사용하는 signup() 메소드 추가

--- server/service/auth.js
+++ server/service/auth.js
@@ -1,7 +1,25 @@
-const { userModel, boardModel } = require('../models');
+const { userModel } = require('../models');
 
 class Auth {
   constructor() {}
+
+  async signup(user) {
+    try {
+      if (!this.validateId(user.id)) {
+        throw new Error(
+          'Id must be a combination of 5 to 20 lower case, number, _ or -.',
+        );
+      } else if (!this.validatePassword(user.password)) {
+        throw new Error(
+          'Password must be between 8 and 16 characters including alphabets, numbers and special characters.',
+        );
+      }
+      const result = await userModel.addUser(user);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
 
   validateId(id) {
     const pattern = /^[a-z0-9][a-z0-9\-_]{4,19}$/;
