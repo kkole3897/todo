e925dd3c6fab954f2d6eab03c0112880b5abcf63
KimJinKwan <kkole3897@naver.com>
2021-02-14 15:45:03 +0900

    feat: id, password 형식 체크 기능 추가
    - id와 password를 입력했을 때 올바른 형식으로 입력했는지 확인하는 기능
    추가
    - 잘못된 형식으로 입력했을 때 입력 부분의 색을 빨간색으로 변경해서 알림
    - id, password 올바른 형식일 때만 제출 버튼 활성화

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,14 +1,105 @@
 import './SignUpForm.css';
+import { debounce } from '../util';
 
 function SignUpForm() {
   const $signUpForm = document.createElement('form');
   const $idBox = document.createElement('div');
-  const $idInputLabel = document.createElement('label');
+  const $idLabel = document.createElement('label');
   const $idInput = document.createElement('input');
   const $passwordBox = document.createElement('div');
-  const $passwordInputLabel = document.createElement('label');
+  const $passwordLabel = document.createElement('label');
   const $passwordInput = document.createElement('input');
   const $submitButton = document.createElement('button');
+
+  const idInputWaitingTime = 500;
+  const passwordInputWaitingTime = 500;
+  let isIdProper = false,
+    isPasswordProper = false;
+
+  const onIdChanged = event => {
+    const pattern = /^[a-z0-9][a-z0-9\-_]{4,19}$/;
+    if (!pattern.test($idInput.value)) {
+      setIsIdProper(false);
+      return;
+    }
+    // TODO: id 중복 확인
+    setIsIdProper(true);
+  };
+
+  const onPasswordChanged = event => {
+    const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[~`!@#$%^&*()\-_=+{}\[\]\\|:;'",<.>/?]).{8,16}$/;
+    if (!pattern.test($passwordInput.value)) {
+      setIsPasswordProper(false);
+      return;
+    }
+    setIsPasswordProper(true);
+  };
+
+  const setIsIdProper = value => {
+    isIdProper = value;
+    handleSubmitbuttonActivation();
+    handleIdInputError();
+    // TODO: id 형식 안내 메시지 띄우기
+  };
+
+  const setIsPasswordProper = value => {
+    isPasswordProper = value;
+    handleSubmitbuttonActivation();
+    handlePasswordInputError();
+    // TODO: password 형식 안내 메시지 띄우기
+  };
+
+  const handleSubmitbuttonActivation = () => {
+    $submitButton.disabled = !(isIdProper && isPasswordProper);
+  };
+
+  const handleIdInputError = () => {
+    const idLabelErrorClassName = 'sign-up-form__label--error';
+    const idInputErrorClassName = 'sign-up-form__input--error';
+    const idLabelClasses = $idLabel.className.split(' ');
+    const idInputClasses = $idInput.className.split(' ');
+    if (isIdProper) {
+      $idLabel.className = idLabelClasses
+        .filter(className => className != idLabelErrorClassName)
+        .join(' ');
+      $idInput.className = idInputClasses
+        .filter(className => className != idInputErrorClassName)
+        .join(' ');
+    } else {
+      $idLabel.className = !idLabelClasses.includes(idLabelErrorClassName)
+        ? [...idLabelClasses, idLabelErrorClassName].join(' ')
+        : $idLabel.className;
+      $idInput.className = !idInputClasses.includes(idInputErrorClassName)
+        ? [...idInputClasses, idInputErrorClassName].join(' ')
+        : $idInput.className;
+    }
+  };
+
+  const handlePasswordInputError = () => {
+    const passwordLabelErrorClassName = 'sign-up-form__label--error';
+    const passwordInputErrorClassName = 'sign-up-form__input--error';
+    const passwordLabelClasses = $passwordLabel.className.split(' ');
+    const passwordInputClasses = $passwordInput.className.split(' ');
+    if (isPasswordProper) {
+      $passwordLabel.className = passwordLabelClasses
+        .filter(className => className != passwordLabelErrorClassName)
+        .join(' ');
+      $passwordInput.className = passwordInputClasses
+        .filter(className => className != passwordInputErrorClassName)
+        .join(' ');
+    } else {
+      $passwordLabel.className = !passwordLabelClasses.includes(
+        passwordLabelErrorClassName,
+      )
+        ? [...passwordLabelClasses, passwordLabelErrorClassName].join(' ')
+        : $passwordLabel.className;
+      $passwordInput.className = !passwordInputClasses.includes(
+        passwordInputErrorClassName,
+      )
+        ? [...passwordInputClasses, passwordInputErrorClassName].join(' ')
+        : $passwordInput.className;
+    }
+  };
 
   $signUpForm.className = 'sign-up-form';
   $signUpForm.setAttribute('autocomplete', 'off');
@@ -17,30 +108,36 @@ function SignUpForm() {
 
   $passwordBox.className = 'box--column';
 
-  $idInputLabel.className = 'sign-up-form__label';
-  $idInputLabel.innerHTML = 'Id';
-  $idInputLabel.htmlFor = 'id';
+  $idLabel.className = 'sign-up-form__label';
+  $idLabel.innerHTML = 'Id';
+  $idLabel.htmlFor = 'id';
 
   $idInput.className = 'sign-up-form__input sign-up-form__input--mt';
   $idInput.setAttribute('type', 'text');
   $idInput.id = 'id';
+  $idInput.addEventListener('input', debounce(onIdChanged, idInputWaitingTime));
 
-  $passwordInputLabel.className = 'sign-up-form__label';
-  $passwordInputLabel.innerHTML = 'Password';
-  $passwordInputLabel.htmlFor = 'password';
+  $passwordLabel.className = 'sign-up-form__label';
+  $passwordLabel.innerHTML = 'Password';
+  $passwordLabel.htmlFor = 'password';
 
   $passwordInput.className = 'sign-up-form__input sign-up-form__input--mt';
   $passwordInput.setAttribute('type', 'password');
   $passwordInput.id = 'password';
+  $passwordInput.addEventListener(
+    'input',
+    debounce(onPasswordChanged, passwordInputWaitingTime),
+  );
 
   $submitButton.className =
     'sign-up-form__submit-button sign-up-form__submit-button--mt';
   $submitButton.innerHTML = 'Create account';
+  $submitButton.disabled = true;
 
-  $idBox.appendChild($idInputLabel);
+  $idBox.appendChild($idLabel);
   $idBox.appendChild($idInput);
 
-  $passwordBox.appendChild($passwordInputLabel);
+  $passwordBox.appendChild($passwordLabel);
   $passwordBox.appendChild($passwordInput);
 
   $signUpForm.appendChild($idBox);
