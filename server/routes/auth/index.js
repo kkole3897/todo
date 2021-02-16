95d27678d6848d3673dc80aa9d27936c8c39416b
kkole3897 <kkole3897@naver.com>
2021-02-16 20:40:05 +0900

    feat: 사용하지 않는 routes 삭제
    - auth, card 관련 api를 다시 정의할 것이기 때문에 삭제

--- server/routes/auth/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const login = require('./login');
-const logout = require('./logout');
-
-router.use('/login', login);
-router.use('/logout', logout);
-
-module.exports = router;
--- server/routes/card/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const card = require('./card');
-
-router.get('/', card.getCards);
-router.post('/', card.addCard);
-router.put('/', card.updateCard);
-
-module.exports = router;
