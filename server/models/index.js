870b2183a7dcdebd609b748144590bc9e161572e
kkole3897 <kkole3897@naver.com>
2021-02-19 16:59:46 +0900

    feat: DB_NAME 환경 변수 추가
    - 데이터베이스 이름도 환경변수로 접근하도록 변경
    - 보안상 환경변수로 숨기는 것이 더욱 낫고, 이후 테스트 환경에서 다른
    db로 접근할 수 있도록 하기 위해서 변경

--- server/models/index.js
+++ server/models/index.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'todo_test',
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
