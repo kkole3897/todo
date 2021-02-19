e4286fcd0a1b5976f347660da43d6c5e9134e990
kkole3897 <kkole3897@naver.com>
2021-02-19 21:30:02 +0900

    feat: Board Service 생성
    - api와 model 사이에서 비즈니스 로직을 수행하는 Board Service 추가

---
+++ server/service/board.js
@@ -0,0 +1,64 @@
+const { boardModel } = require('../models');
+
+class Board {
+  constructor() {}
+
+  async readBoards(userId) {
+    try {
+      const boards = await boardModel.getBoardsByUser(userId);
+      return boards.map(board => {
+        return { ...board };
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async readBoard(id) {
+    try {
+      const board = await boardModel.getBoardById(id);
+      return board ? { ...board } : null;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async createBoard({ userId, name }) {
+    try {
+      if (!this.validateBoardName(name)) {
+        throw new Error('Name of board must be between 1 to 50 characters.');
+      }
+      const result = await boardModel.addBoard({ userId, name });
+      return { ...result };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateBoard({ id, name }) {
+    try {
+      if (!this.validateBoardName(name)) {
+        throw new Error('Name of board must be between 1 to 50 characters.');
+      }
+      const result = await boardModel.updateBoard({ id, name });
+      return { ...result };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async deleteBoard(id) {
+    try {
+      const result = await boardModel.deleteBoard(id);
+      return { ...result };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  validateBoardName(name) {
+    return 0 < name.length && name.length <= 50;
+  }
+}
+
+module.exports = Board;
