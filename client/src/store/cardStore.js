3275b225247fc6f9fa0fb23a7ff557615f6873b4
kkole3897 <kkole3897@naver.com>
2021-03-11 15:11:56 +0900

    feat: delete action 추가
    - 일치하는 id를 찾아서 store에서 제거

--- client/src/store/cardStore.js
+++ client/src/store/cardStore.js
@@ -4,6 +4,7 @@ const ACTION_INIT_CARDS = 'ACTION_INIT_CARDS';
 const ACTION_ADD_CARDS = 'ACTION_ADD_CARDS';
 const ACTION_ADD_CARD = 'ACTION_ADD_CARD';
 const ACTION_UPDATE_DESCRIPTION = 'ACTION_UPDATE_DESCRIPTION';
+const ACTION_DELETE_CARD = 'ACTION_DELETE_CARD';
 
 function reducer(state = {}, { type, payload }) {
   switch (type) {
@@ -30,6 +31,11 @@ function reducer(state = {}, { type, payload }) {
           description: payload.description,
         }),
       };
+    case ACTION_DELETE_CARD:
+      return {
+        ...state,
+        cards: deleteCard(state.cards, payload.id),
+      };
     default:
       return {
         ...state,
@@ -44,6 +50,10 @@ function updateDescription(cards, { id, description }) {
     }
     return card;
   });
+}
+
+function deleteCard(cards, id) {
+  return cards.filter(card => card.id !== id);
 }
 
 const cardStore = createStore({ initialState: { cards: [] }, reducer });
