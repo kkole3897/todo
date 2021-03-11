2aaf6a027478006ddf72a33cf1b71ab18e19c03e
kkole3897 <kkole3897@naver.com>
2021-03-12 01:48:22 +0900

    feat: ACTION_UPDATE_NAME 추가
    - board의 name을 업데이트하는 액션 추가

--- client/src/store/boardStore.js
+++ client/src/store/boardStore.js
@@ -2,6 +2,7 @@ import { createStore } from '../lib/todox';
 
 const ACTION_INIT_BOARDS = 'ACTION_INIT_BOARDS';
 const ACTION_ADD_BOARD = 'ACTION_ADD_BOARD';
+const ACTION_UPDATE_NAME = 'ACTION_UPDATE_NAME';
 
 function reducer(state = {}, { type, payload }) {
   switch (type) {
@@ -15,11 +16,28 @@ function reducer(state = {}, { type, payload }) {
         ...state,
         boards: [...state.boards, payload.board],
       };
+    case ACTION_UPDATE_NAME:
+      return {
+        ...state,
+        boards: updateName(state.boards, {
+          id: payload.id,
+          name: payload.name,
+        }),
+      };
     default:
       return {
         ...state,
       };
   }
+}
+
+function updateName(boards, { id, name }) {
+  return boards.map(board => {
+    if (board.id === id) {
+      board.name = name;
+    }
+    return board;
+  });
 }
 
 const boardStore = createStore({ initialState: { boards: [] }, reducer });
