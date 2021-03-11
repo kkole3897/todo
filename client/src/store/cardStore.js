90786ea1a72d1cd07270e717fcd954e2981ebcc6
kkole3897 <kkole3897@naver.com>
2021-03-11 14:29:52 +0900

    feat: update description action 생성
    - card id로 필터링한 후 description을 업데이트하는 action 추가
    - card의 description을 업데이트하는 event에서 사용하기 위해서 생성

--- client/src/store/cardStore.js
+++ client/src/store/cardStore.js
@@ -3,6 +3,7 @@ import { createStore } from '../lib/todox';
 const ACTION_INIT_CARDS = 'ACTION_INIT_CARDS';
 const ACTION_ADD_CARDS = 'ACTION_ADD_CARDS';
 const ACTION_ADD_CARD = 'ACTION_ADD_CARD';
+const ACTION_UPDATE_DESCRIPTION = 'ACTION_UPDATE_DESCRIPTION';
 
 function reducer(state = {}, { type, payload }) {
   switch (type) {
@@ -21,11 +22,28 @@ function reducer(state = {}, { type, payload }) {
         ...state,
         cards: [payload.card, ...state.cards],
       };
+    case ACTION_UPDATE_DESCRIPTION:
+      return {
+        ...state,
+        cards: updateDescription(state.cards, {
+          id: payload.id,
+          description: payload.description,
+        }),
+      };
     default:
       return {
         ...state,
       };
   }
+}
+
+function updateDescription(cards, { id, description }) {
+  return cards.map(card => {
+    if (card.id === id) {
+      card.description = description;
+    }
+    return card;
+  });
 }
 
 const cardStore = createStore({ initialState: { cards: [] }, reducer });
